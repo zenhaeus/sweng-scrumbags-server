2f6eb0b0c528e4617e7a89520054e17b7ee1ff8a
Arno Schneuwly <arno.schneuwly@epfl.ch>
2014-11-03 19:55:04 +0100

    Updated Client IDS

--- ScrumToolAppEngine/src/ch/epfl/scrumtool/server/Constants.java
+++ ScrumToolAppEngine/src/ch/epfl/scrumtool/server/Constants.java
@@ -7,10 +7,9 @@ package ch.epfl.scrumtool.server;
  * @author aschneuw
  *
  */
-public class Constants {
-	public static final String WEB_CLIENT_ID = 
-	        "756445222019-sefvcpr9aos0dg4r0unt44unvaqlg1nq.apps.googleusercontent.com";
-	public static final String ANDROID_CLIENT_ID = WEB_CLIENT_ID; 
+public class Constants {        
+	public static final String WEB_CLIENT_ID = "756445222019-7ll8hq36aorbfbno1unp49ikle7kt1nv.apps.googleusercontent.com";
+	public static final String ANDROID_CLIENT_ID = "756445222019-p1lggd5881u5c9os9vtil4etag05472v.apps.googleusercontent.com"; 
 	public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 	public static final String EMPTY_STRING = "";
 }
