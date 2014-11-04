dc3984a2764c9f800cc8a68d6a1d1ffe5af37732
Arno <arno.schneuwly@epfl.ch>
2014-11-04 10:10:09 +0100

    Updated Client IDs

--- ScrumToolAppEngine/src/ch/epfl/scrumtool/server/Constants.java
+++ ScrumToolAppEngine/src/ch/epfl/scrumtool/server/Constants.java
@@ -11,6 +11,10 @@ public class Constants {
     public static final String WEB_CLIENT_ID = "756445222019-7ll8hq36aorbfbno1unp49ikle7kt1nv.apps.googleusercontent.com";
     public static final String ANDROID_CLIENT_ID_ARNO_MACBOOK = "756445222019-p1lggd5881u5c9os9vtil4etag05472v.apps.googleusercontent.com";
     public static final String ANDROID_CLIENT_ID_JOEY_DESKTOP = "756445222019-2mq78dl6qp4h5dmg0bjbqp019q848m5h.apps.googleusercontent.com";
+    public static final String ANDROID_CLIENT_ID_LORIS_MACBOOK = "756445222019-v03m2l0gph4aim5ocj7fpf2i8m2tjlcr.apps.googleusercontent.com";
+    public static final String ANDROID_CLIENT_ID_VINCENT_THINKPAD = "756445222019-m7bn58gmbm3qj4h1vkhtd338fj6pgcgi.apps.googleusercontent.com";
+    public static final String ANDROID_CLIENT_ID_SYLVAIN_THINKPAD = "756445222019-imoc5kbegd7uacf6lr3a6j85r8vpu08n.apps.googleusercontent.com";
+    public static final String ANDROID_CLIENT_ID_ALEX_MACBOOK = "756445222019-imoc5kbegd7uacf6lr3a6j85r8vpu08n.apps.googleusercontent.com";
     public static final String[] ANDROID_CLIENT_IDS = 
             {ANDROID_CLIENT_ID_ARNO_MACBOOK,  ANDROID_CLIENT_ID_JOEY_DESKTOP};
     public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
