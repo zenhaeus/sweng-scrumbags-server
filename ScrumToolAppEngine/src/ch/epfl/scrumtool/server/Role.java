2ade5f9840be134c9832d6cb971cd0f8df3d7bec
Vincent Débieux <vincent.debieux@epfl.ch>
2014-12-06 16:01:40 +0100

    remove unused INVITED role

--- ScrumToolAppEngine/src/ch/epfl/scrumtool/server/Role.java
+++ ScrumToolAppEngine/src/ch/epfl/scrumtool/server/Role.java
@@ -6,5 +6,5 @@ package ch.epfl.scrumtool.server;
  * @author ketsio, aschneuw, zenhaeus
  */
 public enum Role {
-    PRODUCT_OWNER, STAKEHOLDER, SCRUM_MASTER, DEVELOPER, INVITED;
+    PRODUCT_OWNER, STAKEHOLDER, SCRUM_MASTER, DEVELOPER;
 }
